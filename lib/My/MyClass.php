497a68708075a64f493b609fdb2e187b0e5884a2
User 77web <77web@www1389.sakura.ne.jp>
2011-11-24 15:54:51 +0900

    add sample plugin and dummy class

---
+++ lib/My/MyClass.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace My;
+
+class MyClass
+{
+  //do something
+}
--- wp-sf-myplugin.php
+++ wp-sf-myplugin.php
@@ -14,3 +14,22 @@ $loader->registerNamespace('Symfony\Component', __DIR__.'/lib/');
 $loader->registerNamespace('My', __DIR__.'/lib/');
 $loader->register();
 
+
+$messages = array('asa'=>'おはようございます。', 'hiru'=>'こんにちは！', 'yoru'=>'こんばんは。');
+$translationMessageSelector = new SFComponent\Translation\MessageSelector();
+$translationLoader = new SFComponent\Translation\Loader\ArrayLoader();
+$translator = new SFComponent\Translation\Translator('ja', $translationMessageSelector);
+$translator->addLoader('array', $translationLoader);
+$translator->addResource('array', $messages, 'ja');
+function wpmyplugin_add_greeting($content)
+{
+  date_default_timezone_set('Asia/Tokyo');
+  $hour = date('G');
+  $greetingType = ($hour > 4 && $hour < 11) ? 'asa' :(($hour >10 && $hour < 16) ?  'hiru' : 'yoru');
+  
+  global $translator;
+  $greeting = $translator->trans($greetingType);
+  return '<p id="greeting">'.$greeting.'</p>'.$content;
+}
+
+add_filter('the_content', 'wpmyplugin_add_greeting');
